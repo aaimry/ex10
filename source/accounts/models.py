0b1f372c8f7b392e15472ec0096d65559d65dfde
Mariya <wheal28@gmail.com>
2022-06-25 10:21:06 +0600

    Создала новую миграцию и применила ее

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.models import AbstractUser
+from django.urls import reverse
 from phonenumber_field.modelfields import PhoneNumberField
 from django.utils.translation import gettext as _
 
 
 class User(AbstractUser):
     phone = PhoneNumberField(region="KG", max_length=13, verbose_name=_('Номер телефона'))
+
+    def get_absolute_url(self):
+        return reverse('accounts:profile', kwargs={'pk': self.pk})
