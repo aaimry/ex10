242b1a177f8c729ab3810c2232e05c7256d397a8
Mariya <wheal28@gmail.com>
2022-06-25 10:03:45 +0600

    Написала модель User унаследованную от AbstractUser

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,3 +1,7 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
+from phonenumber_field.modelfields import PhoneNumberField
+from django.utils.translation import gettext as _
 
-# Create your models here.
+
+class User(AbstractUser):
+    phone = PhoneNumberField(region="KG", max_length=13, verbose_name=_('Номер телефона'))
